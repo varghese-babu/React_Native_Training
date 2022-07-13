bedce1126dcbadb94c4f0eb69624741183613fa0
Varghese Babu <varghese@keyvalue.systems>
2022-07-13 18:18:59 +0530

    index modified

--- src/components/index.ts
+++ src/components/index.ts
@@ -1,3 +1,6 @@
 import Button from './button/Button';
+import Header from './header/header';
+import DropDown from './dropdown/dropdown';
+import EmployeeCard from './employeeCard/employeeCard';
 
-export { Button };
+export { Button, Header, DropDown, EmployeeCard };
