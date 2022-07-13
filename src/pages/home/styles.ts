d477ccf3b1df48e5a41f3dd7cd7a0e449ade44f9
Varghese Babu <varghese@keyvalue.systems>
2022-07-13 18:14:29 +0530

    home page style added

--- src/pages/home/styles.ts
+++ src/pages/home/styles.ts
@@ -1,33 +1,52 @@
 import { ScaledSheet } from 'react-native-size-matters';
 import { colors, fonts, globalStyles } from '@themes';
+import { ListIcon } from '@assets/icons';
 
 const styles = ScaledSheet.create({
   container: {
-    flex: 1
-  },
-  title: {
-    ...fonts.medium,
-    fontWeight: 'bold',
-    paddingTop: '15@vs'
-  },
-  body: {
-    ...fonts.small,
-    paddingTop: '15@vs'
-  },
-  titleWrapper: {
-    marginTop: '50@vs',
-    paddingHorizontal: '20@s'
-  },
-  bodyWrapper: {
-    height: '60%',
-    ...globalStyles.center
-  },
-  heading: {
-    color: colors.TRANSPARENT_BLACK_1
-  },
-  spinner: {
     flex: 1,
-    ...globalStyles.center
+    backgroundColor: '#ffffff'
+  },
+
+  listTitle: {
+    height: '53@vs',
+    flexDirection: 'row',
+    alignItems: 'center',
+    justifyContent: 'space-between'
+  },
+
+  listHeadText: {
+    marginLeft: '20@s',
+    marginTop: '15@vs',
+    marginBottom: '16@vs',
+    fontWeight: 'bold',
+    color: '#000000'
+  },
+
+  profileHead: {
+    height: '56@vs',
+    flexDirection: 'row',
+    alignItems: 'center',
+    backgroundColor: '#eaf9ff'
+  },
+
+  nameTitleStyle: {
+    marginLeft: '20@s',
+    fontFamily: 'Nunito Sans',
+    fontStyle: 'normal',
+    fontWeight: '400',
+    fontSize: 16,
+    lineHeight: 22
+  },
+
+  statusTitleStyle: {
+    marginLeft: '55@s',
+    marginRight: '137@s',
+    fontFamily: 'Nunito Sans',
+    fontStyle: 'normal',
+    fontWeight: '400',
+    fontSize: 16,
+    lineHeight: 22
   }
 });
 
