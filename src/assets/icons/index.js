62b2fc1ae7fe892c1b3684849f4de2a8de2e50a7
Varghese Babu <varghese@keyvalue.systems>
2022-07-13 18:19:47 +0530

    icons added

--- src/assets/icons/index.js
+++ src/assets/icons/index.js
@@ -1,3 +1,9 @@
 import SampleIcon from './icon.svg';
+import HamburgerIcon from './hamburger.svg';
+import ListIcon from './listIcon.svg';
+import PolygonIcon from './polygon.svg';
+import EditIcon from './edit.svg'
+import EditPencilIcon from './editPencil.svg';
+import DeleteIcon from './delete.svg';
 
-export { SampleIcon };
+export { SampleIcon, HamburgerIcon, ListIcon, PolygonIcon, EditIcon, EditPencilIcon, DeleteIcon};
