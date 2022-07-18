f61d30f10d47e8228efa7b60d3b1bc1df18d39cf
Varghese Babu <varghese@keyvalue.systems>
2022-07-18 14:03:39 +0530

    added to index to export svg

--- src/assets/icons/index.js
+++ src/assets/icons/index.js
@@ -2,8 +2,18 @@ import SampleIcon from './icon.svg';
 import HamburgerIcon from './hamburger.svg';
 import ListIcon from './listIcon.svg';
 import PolygonIcon from './polygon.svg';
-import EditIcon from './edit.svg'
+import EditIcon from './edit.svg';
 import EditPencilIcon from './editPencil.svg';
 import DeleteIcon from './delete.svg';
+import FloatingIcon from './floating.svg';
 
-export { SampleIcon, HamburgerIcon, ListIcon, PolygonIcon, EditIcon, EditPencilIcon, DeleteIcon};
+export {
+  SampleIcon,
+  HamburgerIcon,
+  ListIcon,
+  PolygonIcon,
+  EditIcon,
+  EditPencilIcon,
+  DeleteIcon,
+  FloatingIcon
+};
